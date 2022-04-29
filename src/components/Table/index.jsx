9f1def60b82da375b82ce35249a47cbc9bacaa13
DAVI PEREIRA A B CARDOSO <730549526@adm.unifor.br>
2022-04-29 16:32:16 -0300

    Update

---
+++ src/components/Table/index.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+import useTable from "../useTable";
+import styles from "./Table.module.css";
+import TableFooter from "./TableFooter";
+import { Button } from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
+import { AiFillDelete, AiOutlineSearch } from 'react-icons/ai';
+import api from '../../services/api'
+
+function Deletar(el) {
+
+  const headers = {
+      'Authorization': "Bearer " + sessionStorage.getItem('accessToken'),
+  }
+
+  let id = el._id
+
+  console.log(id)
+
+  api.delete(`/admin/${id}`, { headers: headers })
+
+      .then(res => {
+
+          toast.success("Usuário deletado!")
+          setTimeout(() =>
+              document.location.reload(), 3000
+          )
+
+      }).catch(err => {
+          console.log('erro', err)
+      })
+};
+
+const Table = ({ data, rowsPerPage }) => {
+  const [page, setPage] = useState(1);
+  const { slice, range } = useTable(data, page, rowsPerPage);
+  return (
+    <div>
+      <table className={styles.table}>
+        <thead className={styles.tableRowHeader}>
+          <tr>
+            <th className={styles.tableHeader}>Nome</th>
+            <th className={styles.tableHeader}>Matrícula</th>
+            <th className={styles.tableHeader}>CPF</th>
+            <th className={styles.tableHeader}>Email</th>
+            <th className={styles.tableHeader}>Tipo de Usuário</th>
+            <th className={styles.tableHeader}>Ação</th>
+          </tr>
+        </thead>
+        <tbody>
+          {slice.map((el) => (
+            <tr className={styles.tableRowItems} key={el.id}>
+              <td className={styles.tableCell}>{el.name}</td>
+              <td className={styles.tableCell}>{el.matriculation}</td>
+              <td className={styles.tableCell}>{el.cpfUser}</td>
+              <td className={styles.tableCell}>{el.email}</td>
+              <td className={styles.tableCell}>{el.typeUser}</td>
+              <Button variant="danger" onClick={(e) => Deletar(el, e)}> <AiFillDelete></AiFillDelete> </Button>
+              {/* <td className={styles.tableCell}>{el.cpfUser}</td> */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <TableFooter range={range} slice={slice} setPage={setPage} page={page} />
+    </div>
+  );
+};
+
+export default Table;
--- src/components/pages/UserDashboard/Users.jsx
+++ src/components/pages/UserDashboard/Users.jsx
@@ -1,43 +1,45 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import "../../../assets/css/Global.scss"
 import HeaderNavigator from '../HeaderNavigator/HeaderNavigator.jsx'
 import api from '../../../services/api.js'
 import 'react-toastify/dist/ReactToastify.css';
-import {Button} from "react-bootstrap"
+import { Button } from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { toast } from 'react-toastify';
-import {AiFillDelete, AiOutlineSearch} from 'react-icons/ai';
+import { AiFillDelete, AiOutlineSearch } from 'react-icons/ai';
+import styles from "./App.module.css";
+import Table from "../../Table";
 
 export default function Users() {
 
     const [users, setUsers] = useState([]);
     const [search, setSearch] = useState("");
-    
-    useEffect(() =>{
-        
+
+    useEffect(() => {
+
         const headers = {
             'Authorization': "Bearer " + sessionStorage.getItem('accessToken'),
         }
 
         api.get(`admin`, { headers: headers })
-        
-            .then(res =>{
-        
+
+            .then(res => {
+
                 console.log(res.data)
                 setUsers(res.data)
-        
+
             }).catch(err => {
-            console.log(err)
+                console.log(err)
             })
 
     }, [])
 
-    function Deletar(user) {  
+    function Deletar(user) {
 
         const headers = {
             'Authorization': "Bearer " + sessionStorage.getItem('accessToken'),
-            }
+        }
 
         let id = user._id
 
@@ -45,82 +47,87 @@ export default function Users() {
 
         api.delete(`/admin/${id}`, { headers: headers })
 
-        .then(res =>{
-            
-            toast.success("Usuário deletado!")
-            setTimeout(() =>
-            document.location.reload(), 3000
-            )
+            .then(res => {
 
-        }).catch(err => {
-        console.log('erro',err)
-        })
-      };
+                toast.success("Usuário deletado!")
+                setTimeout(() =>
+                    document.location.reload(), 3000
+                )
 
-      function updateSearch (event) {
+            }).catch(err => {
+                console.log('erro', err)
+            })
+    };
+
+    function updateSearch(event) {
         setSearch(event.target.value.substr(0, 20))
-      }
-    
-      const searchUsers = users.filter(
-        (user) => {
-          return (user.name).indexOf(search) ===  0 || (user.cpfUser).indexOf(search) ===  0;
-        }
-      )
+    }
 
-  return (
-    <div className = '__backgroundUsersList'>
-        <div>
-            <HeaderNavigator></HeaderNavigator>
-            <div className = '__divTitle'>
-                <text className = '__title'>Lista de usuários</text>
-                <div>__________________________________________</div>
-            </div>
-            <div style = {{display: 'flex', justifyContent: 'flex-end', alignItems: 'center', padding: "0px 48px 0px 0px"}}>
-                <input type = 'text' placeholder = 'Pesquisar usuário' className = 'inputSearch' onChange={updateSearch} value = {search} style = {{marginLeft: "20px"}}></input>
-                <div style = {{backgroundColor: "gray", height: "3vh", width: "2vw", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "2px"}}>
-                    <AiOutlineSearch></AiOutlineSearch>
+    const searchUsers = users.filter(
+        (user) => {
+            return (user.name).indexOf(search) === 0 || (user.cpfUser).indexOf(search) === 0;
+        }
+    )
+
+    return (
+        <div className='__backgroundUsersList'>
+            <div>
+                <HeaderNavigator></HeaderNavigator>
+                <div className='__divTitle'>
+                    <text className='__title'>Lista de usuários</text>
+                    <div>__________________________________________</div>
+                </div>
+                <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', padding: "0px 48px 0px 0px" }}>
+                    <input type='text' placeholder='Pesquisar usuário' className='inputSearch' onChange={updateSearch} value={search} style={{ marginLeft: "20px" }}></input>
+                    <div style={{ backgroundColor: "gray", height: "3vh", width: "2vw", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "2px" }}>
+                        <AiOutlineSearch></AiOutlineSearch>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className = '__usersList'>
-            <table id = "data">
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Matrícula</th>
-                        <th>CPF</th>
-                        <th>Email</th>
-                        <th>Tipo de usuário</th>
-                        <th>Ação</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {searchUsers.map(user =>
-                        <tr key = {user._id}>
-                            <td>
-                                {user.name}
-                            </td>
-                            <td>
-                                {user.matriculation}
-                            </td>
-                            <td>
-                                {user.cpfUser}
-                            </td>
-                            <td>
-                                {user.email}
-                            </td>
-                            <td>
-                                {user.typeUser}
-                            </td>
-                            <td>
-                                <Button variant="danger" onClick = {(e) => Deletar(user,e)}> <AiFillDelete></AiFillDelete> </Button>
-                            </td>
+            {/* <div className='__usersList'> */}
+                {/* <table id="data">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Matrícula</th>
+                            <th>CPF</th>
+                            <th>Email</th>
+                            <th>Tipo de usuário</th>
+                            <th>Ação</th>
                         </tr>
-                    )}
+                    </thead>
+                    <tbody>
+                        {searchUsers.map(user =>
+                            <tr key={user._id}>
+                                <td>
+                                    {user.name}
+                                </td>
+                                <td>
+                                    {user.matriculation}
+                                </td>
+                                <td>
+                                    {user.cpfUser}
+                                </td>
+                                <td>
+                                    {user.email}
+                                </td>
+                                <td>
+                                    {user.typeUser}
+                                </td>
+                                <td>
+                                    <Button variant="danger" onClick={(e) => Deletar(user, e)}> <AiFillDelete></AiFillDelete> </Button>
+                                </td>
+                            </tr>
+                        )}
 
-                </tbody>
-            </table>
-        </div>
-    </div>
-  )
+                    </tbody>
+                </table> */}
+                <main className={styles.container}>
+                    <div className={styles.wrapper}>
+                        <Table data={searchUsers} rowsPerPage={1} />
+                    </div>
+                </main>
+            </div>
+        // </div>
+    )
 }
