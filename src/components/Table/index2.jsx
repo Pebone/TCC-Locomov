f5e78dfb1dbcff4e834cc7b6ffaddab4e98d8bbd
DAVI PEREIRA A B CARDOSO <730549526@adm.unifor.br>
2022-04-29 17:24:07 -0300

    Update

---
+++ src/components/Table/index2.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+import useTable from "../useTable";
+import styles from "./Table.module.css";
+import TableFooter from "./TableFooter";
+import { Button } from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
+import { AiFillDelete, AiOutlineSearch } from 'react-icons/ai';
+import api from '../../services/api'
+
+function Deletar(el) {
+
+  const headers = {
+      'Authorization': "Bearer " + sessionStorage.getItem('accessToken'),
+  }
+
+  let id = el._id
+
+  console.log(id)
+
+  api.delete(`/admin/scooter/${id}`, { headers: headers })
+
+      .then(res => {
+
+          toast.success("Patinete deletado!")
+          setTimeout(() =>
+              document.location.reload(), 3000
+          )
+
+      }).catch(err => {
+          console.log('erro', err)
+      })
+};
+
+const Table = ({ data, rowsPerPage }) => {
+  const [page, setPage] = useState(1);
+  const { slice, range } = useTable(data, page, rowsPerPage);
+  return (
+    <div>
+      <table className={styles.table}>
+        <thead className={styles.tableRowHeader}>
+          <tr>
+            <th className={styles.tableHeader}>Código</th>
+            <th className={styles.tableHeader}>Localização</th>
+            <th className={styles.tableHeader}>Bateria</th>
+            <th className={styles.tableHeader}>Status</th>
+            {/* <th className={styles.tableHeader}>Tipo de Usuário</th> */}
+            <th className={styles.tableHeader}>Ação</th>
+          </tr>
+        </thead>
+        <tbody>
+          {slice.map((el) => (
+            <tr className={styles.tableRowItems} key={el.id}>
+              <td className={styles.tableCell}>{el.code}</td>
+              <td className={styles.tableCell}>{el.localization}</td>
+              <td className={styles.tableCell}>{el.battery}</td>
+              <td className={styles.tableCell}>{el.status}</td>
+              <Button variant="danger" onClick={(e) => Deletar(el, e)}> <AiFillDelete></AiFillDelete> </Button>
+              {/* <td className={styles.tableCell}>{el.cpfUser}</td> */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <TableFooter range={range} slice={slice} setPage={setPage} page={page} />
+    </div>
+  );
+};
+
+export default Table;
--- src/components/pages/ScooterDashboard/Scooters.jsx
+++ src/components/pages/ScooterDashboard/Scooters.jsx
@@ -1,43 +1,45 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import "../../../assets/css/Global.scss"
 import HeaderNavigator from '../HeaderNavigator/HeaderNavigator.jsx'
 import api from '../../../services/api.js'
 import 'react-toastify/dist/ReactToastify.css';
-import {Button} from "react-bootstrap"
+import { Button } from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { toast } from 'react-toastify';
-import {AiFillDelete, AiOutlineSearch} from 'react-icons/ai';
+import { AiFillDelete, AiOutlineSearch } from 'react-icons/ai';
+import styles from "../../../components/Table";
+import Table from "../../Table/index2";
 
 export default function Scooters() {
 
     const [scooters, setScooters] = useState([]);
     const [search, setSearch] = useState("");
-    
-    useEffect(() =>{
-        
+
+    useEffect(() => {
+
         const headers = {
             'Authorization': "Bearer " + sessionStorage.getItem('accessToken'),
         }
 
         api.get(`/admin/scooter/find`, { headers: headers })
-        
-            .then(res =>{
-        
+
+            .then(res => {
+
                 console.log(res.data)
                 setScooters(res.data)
-        
+
             }).catch(err => {
-            console.log(err)
+                console.log(err)
             })
 
     }, [])
 
-    function Deletar(scooter) {  
+    function Deletar(scooter) {
 
         const headers = {
             'Authorization': "Bearer " + sessionStorage.getItem('accessToken'),
-            }
+        }
 
         let id = scooter._id
 
@@ -45,44 +47,44 @@ export default function Scooters() {
 
         api.delete(`/admin/scooter/${id}`, { headers: headers })
 
-        .then(res =>{
+            .then(res => {
 
-            toast.success("Patinete deletado!")
-            setTimeout(() =>
-            document.location.reload(), 3000
-            )
+                toast.success("Patinete deletado!")
+                setTimeout(() =>
+                    document.location.reload(), 3000
+                )
 
-        }).catch(err => {
-        console.log('erro',err)
-        })
-      };
-    
-      function updateSearch (event) {
+            }).catch(err => {
+                console.log('erro', err)
+            })
+    };
+
+    function updateSearch(event) {
         setSearch(event.target.value.substr(0, 20))
-      }
-    
-      const searchScooters = scooters.filter(
-        (scooter) => {
-          return (scooter.code).indexOf(search) ===  0;
-        }
-      )
+    }
 
-  return (
-    <div className = '__backgroundUsersList'>
-        <div>
-            <HeaderNavigator></HeaderNavigator>
-        </div>
-        <div className = '__divTitle'>
-                <text className = '__title'>Lista de patinetes</text>
+    const searchScooters = scooters.filter(
+        (scooter) => {
+            return (scooter.code).indexOf(search) === 0;
+        }
+    )
+
+    return (
+        <div className='__backgroundUsersList'>
+            <div>
+                <HeaderNavigator></HeaderNavigator>
+            </div>
+            <div className='__divTitle'>
+                <text className='__title'>Lista de patinetes</text>
                 <div>__________________________________________</div>
             </div>
-            <div style = {{display: 'flex', justifyContent: 'flex-end', alignItems: 'center', padding: "0px 48px 0px 0px"}}>
-                <input type = 'text' placeholder = 'Pesquisar usuário' className = 'inputSearch' onChange={updateSearch} value = {search} style = {{marginLeft: "20px"}}></input>
-                <div style = {{backgroundColor: "gray", height: "3vh", width: "2vw", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "2px"}}>
+            <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', padding: "0px 48px 0px 0px" }}>
+                <input type='text' placeholder='Pesquisar usuário' className='inputSearch' onChange={updateSearch} value={search} style={{ marginLeft: "20px" }}></input>
+                <div style={{ backgroundColor: "gray", height: "3vh", width: "2vw", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "2px" }}>
                     <AiOutlineSearch></AiOutlineSearch>
                 </div>
             </div>
-        <div className = '__usersList'>
+            {/* <div className = '__usersList'>
             <table>
                 <thead>
                     <tr>
@@ -116,7 +118,12 @@ export default function Scooters() {
 
                 </tbody>
             </table>
+        </div> */}
+            <main className={styles.container}>
+                <div className={styles.wrapper}>
+                    <Table data={searchScooters} rowsPerPage={1} />
+                </div>
+            </main>
         </div>
-    </div>
-  )
+    )
 }
